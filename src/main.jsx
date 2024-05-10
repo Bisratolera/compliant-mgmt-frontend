729529827faf1f3136069066b4b897f6ee902d64
bisratolera <leultolera@gmail.com>
2024-05-11 02:55:24 +0300

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-// import AdminLogin from './Pages/AdminLogin.jsx'
+import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import UserPage from './Pages/UserPage';
+import AdminPage from './Pages/AdminPage';
+import Department from './Pages/Department';
+import AdminLogin from './Pages/AdminLogin'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <UserPage />, 
+      }, 
+      {
+        path: "/adminlogin",
+        element: <AdminLogin />, 
+      },
+      {
+        path: "/adminpage",
+        element: <AdminPage />,
+      },
+      {
+        path: "/department",
+        element: <Department />,
+      },
+
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    {/* <AdminLogin /> */}
-  </React.StrictMode>,
+     <RouterProvider router={router} />
+  </React.StrictMode>
 )
